7b08e0c5b3f5ee0647c9d9d7f066dc93987438a8
alott2223 <mcdonaldsincorperated@gmail.com>
2026-01-28 17:25:53 +0000

    Revert "Add Cloudflare backend with invite auth and admin API"
    This reverts commit 1ef8a95f71218909d96d2ddacfe65607e9382500.

--- functions/api/logout.js
+++
@@ -1,9 +0,0 @@
-import { clearSession, ok } from './utils.js';
-
-export async function onRequestPost({ request, env }) {
-  const cookie = await clearSession(env, request);
-  return new Response(JSON.stringify({ message: 'Logged out' }), {
-    status: 200,
-    headers: { 'content-type': 'application/json', 'set-cookie': cookie }
-  });
-}
